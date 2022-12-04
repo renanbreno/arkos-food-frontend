e8c79e0dd4069e6deda941d99a43802d627c6ddd
Renan Oliveira <65349948+renanbreno@users.noreply.github.com>
2022-12-04 19:36:54 -0300

    transitions in signin screen and responsive design

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,4 +1,3 @@
-import { Header } from '../components/Header';
 import { ShoppingCartPrimary, ShoppingCartSecondary } from '../assets/ShoppingCart';
 import { Logo } from '../assets/Logo';
 import { Text } from '../components/Text';
@@ -6,6 +5,7 @@ import { TextInput } from '../components/Input';
 import { Button } from '../components/Button';
 import { Envelope, Lock, User } from 'phosphor-react';
 import { FormEvent, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export function Signin() {
     const [isNewRegister, setIsNewRegister] = useState(false);
@@ -17,21 +17,28 @@ export function Signin() {
 
     return (
         <div className='flex h-screen w-screen'>
-            <div className='flex justify-center items-center h-screen w-1/2 bg-white-s+1'>
-                {isNewRegister ? <ShoppingCartSecondary /> : <ShoppingCartPrimary />}
+            <div className='flex justify-center items-center h-screen w-1/2 bg-white-s+1 max-lg:hidden overflow-hidden'>
+                <motion.div animate={{rotate: isNewRegister ? 360 : 0}}>
+                    {isNewRegister ? <ShoppingCartSecondary /> : <ShoppingCartPrimary />}
+                </motion.div>
             </div>
-            <div className='flex justify-center items-center h-screen w-1/2 bg-white'>
-                <form className='w-full max-w-[50%] flex justify-center items-center flex-col gap-4'>
+            <div className='flex justify-center items-center h-screen w-1/2 max-lg:w-full bg-white'>
+                <motion.form className='w-full max-w-[60%] max-lg:max-w-[90%] flex justify-center items-center flex-col gap-4'>
                     <Logo />
-                    {isNewRegister && <label className='w-full'>
-                        <Text>Nome</Text>
-                        <TextInput.Root>
-                            <TextInput.Icon>
-                                <Envelope />
-                            </TextInput.Icon>
-                            <TextInput.Input placeholder='Insira seu nome' />
-                        </TextInput.Root>
-                    </label>}
+                    <AnimatePresence>
+                        {isNewRegister && (
+                            <motion.label initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{delay: 0.2}} className='w-full'>
+                                <Text>Nome</Text>
+                                <TextInput.Root>
+                                    <TextInput.Icon>
+                                        <Envelope />
+                                    </TextInput.Icon>
+                                    <TextInput.Input placeholder='Insira seu nome' />
+                                </TextInput.Root>
+                            </motion.label>
+                        )}
+                    </AnimatePresence>
+
                     <label className='w-full'>
                         <Text>E-mail</Text>
                         <TextInput.Root>
@@ -50,7 +57,9 @@ export function Signin() {
                             <TextInput.Input placeholder='Insira sua senha' />
                         </TextInput.Root>
                     </label>
-                    {isNewRegister && <label className='w-full'>
+                    <AnimatePresence>
+                    {isNewRegister && (
+                        <motion.label initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{delay: 0.2}} className='w-full'>
                         <Text>Confirmação de senha</Text>
                         <TextInput.Root>
                             <TextInput.Icon>
@@ -58,7 +67,10 @@ export function Signin() {
                             </TextInput.Icon>
                             <TextInput.Input placeholder='Confirme sua senha' />
                         </TextInput.Root>
-                    </label>}
+                    </motion.label>
+                    )}
+                    </AnimatePresence>
+                    
                     <Button.Root className='mt-6'>
                         Entrar na plataforma
                     </Button.Root>
@@ -79,7 +91,7 @@ export function Signin() {
                             </Text>
                         </div>
                     </footer>
-                </form>
+                </motion.form>
             </div>
         </div>
     )
