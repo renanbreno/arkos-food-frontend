6d266f3678e9e801d95fbf5fbc6100f079f1a8e1
Renan Oliveira <65349948+renanbreno@users.noreply.github.com>
2022-12-18 18:42:23 -0300

    create a edit profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './styles/global.css'
 import { Signin } from './pages/Signin';
-import { Catalog } from './pages/Catalog';
-import { Cart } from './pages/Cart';
 
 export function App() {
     return (
-        <Cart />
+        <Signin />
     )
 }
--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -1,3 +1,78 @@
+import { Header } from "../components/Header";
+import { Text } from "../components/Text";
+import { TextInput } from "../components/Input";
+import { Envelope, Lock, User } from 'phosphor-react';
+import { Button } from "../components/Button";
+
 export function EditProfile() {
-    
+    return (
+        <div className='flex mt-8 justify-center w-screen h-screen'>
+            <div className='w-full max-w-[90%] flex flex-col items-center'>
+                <Header />
+                <div className="flex gap-2 mt-6">
+
+                    <Text className="text-helpers-error" asChild>
+                        <a href="">Página inicial</a>
+                    </Text>
+                    <Text className="text-secondary-s-1"> {">"} </Text>
+                    <Text asChild>
+                        <a href="">Editar perfil</a>
+                    </Text>
+                </div>
+
+                <form className="flex flex-col gap-4 w-full max-w-[40%] max-lg:max-w-full max:lg justify-center items-center mt-6">
+                    <label className='w-full'>
+                        <Text>Nome completo</Text>
+                        <TextInput.Root>
+                            <TextInput.Icon>
+                                <User />
+                            </TextInput.Icon>
+                            <TextInput.Input name='name' placeholder='Insira seu nome' />
+                        </TextInput.Root>
+                    </label>
+                    <label className='w-full'>
+                        <Text>E-mail atual</Text>
+                        <TextInput.Root>
+                            <TextInput.Icon>
+                                <Envelope />
+                            </TextInput.Icon>
+                            <TextInput.Input name='email' placeholder='Insira seu e-mail' />
+                        </TextInput.Root>
+                    </label>
+                    <label className='w-full'>
+                        <Text>Novo e-mail</Text>
+                        <TextInput.Root>
+                            <TextInput.Icon>
+                                <Envelope />
+                            </TextInput.Icon>
+                            <TextInput.Input name='newEmail' placeholder='Insira seu novo e-mail' />
+                        </TextInput.Root>
+                    </label>
+                    <label className='w-full'>
+                        <Text>Senha atual</Text>
+                        <TextInput.Root>
+                            <TextInput.Icon>
+                                <Lock />
+                            </TextInput.Icon>
+                            <TextInput.Input name='password' placeholder='Insira sua senha' />
+                        </TextInput.Root>
+                    </label>
+                    <label className='w-full'>
+                        <Text>Nova senha</Text>
+                        <TextInput.Root>
+                            <TextInput.Icon>
+                                <Lock />
+                            </TextInput.Icon>
+                            <TextInput.Input name='newPassword' placeholder='Insira sua nova senha' />
+                        </TextInput.Root>
+                    </label>
+
+                    <Button.Root>
+                        Alterar dados
+                    </Button.Root>
+                </form>
+
+            </div>
+        </div>
+    )
 }
